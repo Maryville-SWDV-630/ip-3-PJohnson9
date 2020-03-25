f27e57e9c7cf06c3db3fc3971e1d659706bbd7b5
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2020-03-25 15:59:24 -0400

    Create Superclass.py

---
+++ Superclass.py
@@ -0,0 +1,24 @@
+# Patrick Johnson         3/25/2020 #
+# SWDV 630 3W 20/SP2         Week 3 #
+#####################################
+# Code a generic superclass and at least three subclasses
+# of that superclass, each class needs to have at least
+# 2 attributes and 2 methods.
+
+class Superclass:
+    def __init__(self, members):
+        pass
+    
+    
+class SubclassA(Superclass):
+    pass
+
+
+
+class SubclassB(superclass):
+    pass
+
+
+
+class SubclassC(superclass):
+    pass
